73edd2a67b804f1b0813c2e2d068713261d0fdd5
Gerard <gqueraltferre@gmail.com>
2022-10-20 22:57:51 +0200

    added ModuleProgram::compileShader

--- Engine/Source/ModuleProgram.cpp
+++ Engine/Source/ModuleProgram.cpp
@@ -1,6 +1,8 @@
 #include "ModuleProgram.h"
 #include <fstream>
 
+const std::string ModuleProgram::SHADER_FOLDER_PATH = "";
+
 ModuleProgram::ModuleProgram()
 {
 }
@@ -11,19 +13,19 @@ ModuleProgram::~ModuleProgram()
 
 unsigned int ModuleProgram::CreateProgramFromShaders(std::string vertexShaderName, std::string fragmentShaderName)
 {
-	std::string vertexShaderCode = readFile(vertexShaderName);
-	std::string fragmentShaderCode = readFile(fragmentShaderName);
+	std::string vertexShaderCode = readShaderFile(vertexShaderName);
+	std::string fragmentShaderCode = readShaderFile(fragmentShaderName);
 
 	return 0;
 }
 
-std::string ModuleProgram::readFile(std::string fileName)
+std::string ModuleProgram::readShaderFile(std::string fileName)
 {
 	std::ifstream file;
 	std::string fileContents = "";
 	std::string fileLine;
 
-	file.open(fileName);
+	file.open(SHADER_FOLDER_PATH + fileName);
 
 	if (file.is_open()) {
 		while (std::getline(file, fileLine)) {
@@ -33,4 +35,27 @@ std::string ModuleProgram::readFile(std::string fileName)
 	}
 
 	return fileContents;
+}
+
+GLuint ModuleProgram::compileShader(GLenum shaderType, std::string shaderSource)
+{
+	GLuint shaderID = glCreateShader(shaderType);
+	const char* sourceAsChars = shaderSource.c_str();
+	glShaderSource(shaderID, 1, &sourceAsChars, 0);
+	glCompileShader(shaderID);
+
+	int res = GL_FALSE;
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &res);
+	if (res == GL_FALSE) {
+		int len = 0;
+		glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &len);
+		if (len > 0) {
+			int written = 0;
+			char* info = (char*)malloc(len);
+			glGetShaderInfoLog(shaderID, len, &written, info);
+			LOG("Log Info: %s", info);
+			free(info);
+		}
+	}
+	return shaderID;
 }
--- Engine/Source/ModuleProgram.h
+++ Engine/Source/ModuleProgram.h
@@ -2,6 +2,7 @@
 
 #include "Module.h"
 #include <string>
+#include <glew.h>
 
 class ModuleProgram : public Module
 {
@@ -9,9 +10,12 @@ public:
 	ModuleProgram();
 	~ModuleProgram();
 
+	static const std::string SHADER_FOLDER_PATH;
+
 	unsigned int CreateProgramFromShaders(std::string vertexShaderName, std::string fragmentShaderName);
 
 private:
-	std::string readFile(std::string fileName);
+	std::string readShaderFile(std::string fileName);
+	GLuint compileShader(GLenum shaderType, std::string shaderSource);
 };
 
